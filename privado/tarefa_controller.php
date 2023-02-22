9d1e5135db0311e450c5dcb72f891954ce0869e0
FFilins <felipeclayton007@gmail.com>
2023-02-22 13:16:57 -0300

    implementado inclusão de novas tarefas

--- privado/tarefa_controller.php
+++ privado/tarefa_controller.php
@@ -1,8 +1,24 @@
 <?php 
 
+require "tarefa_model.php";
+require "tarefa_service.php";
+require "conexao.php";
+
+$tarefa = new Tarefa();
+$tarefa->__set('tarefa', $_POST['tarefa']);
+
+$conexao = new Conexao();
+
+$tarefaService = new TarefaService($conexao, $tarefa);
+$tarefaService->inserir();
+
+header('Location: nova_tarefa.php?inclusao=1');
+
 echo '<pre>';
-print_r($_POST);
+print_r($tarefaService);
 echo '</pre>';
+
+
 
 
 
